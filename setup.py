d9ad7ae463f0653e3a6e82c99098f57021797401
Beyang Liu <beyang@sourcegraph.com>
2014-08-04 22:24:13 -0700

    some files

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='python-sample-0',
+    version='0.0.0',
+    url='http://github.com/sgtest/python-sample-0/',
+    py_modules=['mod0'],
+    install_requires=[],
+)
