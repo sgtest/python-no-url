48c81e7c00b80b64ff9f9d4148be0d0ea407db11
Beyang Liu <beyang@sourcegraph.com>
2014-09-25 16:18:16 -0700

    remove uri

--- mod0.py
+++ mod0.py
@@ -1,3 +1,2 @@
-import json
-
-json.dumps([])
+def foo():
+    pass
--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 from setuptools import setup
 
 setup(
-    name='python-sample-0',
+    name='python-no-uri',
     version='0.0.0',
-    url='http://github.com/sgtest/python-sample-0/',
     py_modules=['mod0'],
     install_requires=[],
 )
